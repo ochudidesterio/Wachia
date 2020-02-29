c3438b6cff350289ba025235360c9873b803a697
DESTERIO-RASMENTO <desochudi@gmail.com>
2020-02-29 13:55:38 +0300

    navigation menu -m

--- app/src/main/java/com/example/wachia/MainActivity.java
+++ app/src/main/java/com/example/wachia/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.wachia;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -13,21 +17,24 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-   public static DatabaseHelper myDataBaseHelper;
+   //public static DatabaseHelper myDataBaseHelper;
     public static Button getStarted;
 
 
-    @SuppressLint("RestrictedApi")
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
-        myDataBaseHelper = new DatabaseHelper(this);
+
+
         OnClickinGstartedButton();
 
 
+
     }
+
 
     public void OnClickinGstartedButton(){
         getStarted=(Button)findViewById(R.id.getStarted);
@@ -36,10 +43,9 @@ public class MainActivity extends AppCompatActivity {
         getStarted.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(".details_activity");
+                Intent intent = new Intent(".Drawer");
                 startActivity(intent);
             }
         });
-
-    }
+}
 }
--- app/src/main/java/com/example/wachia/details_activity.java
+++ app/src/main/java/com/example/wachia/details_activity.java
@@ -8,11 +8,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import static com.example.wachia.MainActivity.myDataBaseHelper;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+//import static com.example.wachia.MainActivity.myDataBaseHelper;
 
 public class details_activity extends AppCompatActivity {
     EditText idnumber,phonenumber,email,description;
     Button submit;
+    public static DatabaseReference databaseReference;
+    public static Client client;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,12 +28,34 @@ public class details_activity extends AppCompatActivity {
         description =(EditText)findViewById(R.id.DescriptionView);
         submit =(Button) findViewById(R.id.submit);
 
+        databaseReference= FirebaseDatabase.getInstance().getReference().child("Client");
+
         OnClickSubmit();
     }
     public  void OnClickSubmit(){
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                client = new Client();
+                int idNo= Integer.parseInt(idnumber.getText().toString().trim());
+                int phone = Integer.parseInt(phonenumber.getText().toString().trim());
+
+                client.setIdNumber(idNo);
+                client.setPhoneNumber(phone);
+                client.setEmail(email.getText().toString().trim());
+                client.setDescription(description.getText().toString().trim());
+
+                databaseReference.push().setValue(client);
+
+                idnumber.setText(null);
+                phonenumber.setText(null);
+                email.setText(null);
+                description.setText(null);
+
+                Toast.makeText(details_activity.this, "Data inserted successfully", Toast.LENGTH_SHORT).show();
+                /*
+
                boolean isInserted= myDataBaseHelper.insertData(idnumber.getText().toString(),
                         phonenumber.getText().toString(),
                         email.getText().toString(),
@@ -41,7 +68,7 @@ public class details_activity extends AppCompatActivity {
                idnumber.setText(null);
                phonenumber.setText(null);
                email.setText(null);
-               description.setText(null);
+               description.setText(null);*/
             }
         });
     }
