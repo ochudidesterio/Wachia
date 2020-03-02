d2da2d5b3cc00dde100acb74ea261a6c16fc4197
DESTERIO-RASMENTO <desochudi@gmail.com>
2020-03-02 20:39:03 +0300

    calling activity methods from a fragment -m

---
+++ app/src/main/java/com/example/wachia/menuFragments/ClientFragment.java
@@ -0,0 +1,48 @@
+package com.example.wachia.menuFragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.wachia.R;
+
+public class ClientFragment extends Fragment {
+    private  onClientSubmitSelected submit_listener;
+    private Button submit_client;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.client,container,false);
+        submit_client = view.findViewById(R.id.submit);
+        submit_client.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submit_listener.onSubmitButtonSelected();
+            }
+        });
+         return view;
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if(context instanceof onClientSubmitSelected){
+            submit_listener = (onClientSubmitSelected) context;
+        }else {
+            throw new ClassCastException(context.toString()+" must implement a listener");
+        }
+    }
+
+    public interface onClientSubmitSelected{
+        public void onSubmitButtonSelected();
+    }
+}
---
+++ app/src/main/java/com/example/wachia/menuFragments/ScanFragment.java
@@ -0,0 +1,51 @@
+package com.example.wachia.menuFragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.wachia.R;
+
+import java.util.zip.Inflater;
+
+public class ScanFragment extends Fragment {
+    private  onFragmentScanButtonSelected scan_button_listener;
+    private Button scan_button;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_scanner,container,false);
+        scan_button = view.findViewById(R.id.scan);
+        scan_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scan_button_listener.onScanButtonselected();
+            }
+        });
+        return  view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if(context instanceof onFragmentScanButtonSelected){
+            scan_button_listener = (onFragmentScanButtonSelected) context;
+        }else {
+            throw new ClassCastException(context.toString()+ " must implement listener");
+        }
+
+    }
+
+    public interface onFragmentScanButtonSelected{
+        public void onScanButtonselected();
+
+    }
+}
+
